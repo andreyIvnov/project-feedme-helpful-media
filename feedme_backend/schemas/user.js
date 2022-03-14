e0db006f01efe6b350e3eb1fa7e1ec72e959e5a4
Andrey Ivanov <68381704+andreyIvnov@users.noreply.github.com>
2022-03-14 14:23:13 +0200

    Sanity init, user schame creating

---
+++ feedme_backend/schemas/user.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields:[
+        {
+            name:'userName',
+            title: 'User Name',
+            type: 'string'
+        },
+        {
+            name:'image',
+            title: 'Image',
+            type: 'string'
+        },
+        {
+            name:'location',
+            title: 'Location',
+            type: 'string'
+        }
+    ]
+}
